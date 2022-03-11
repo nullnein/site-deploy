540e0711ec1a783a1b6310fa19cbcc9fb03ec63c
nullnein <mathis@oija.de>
2022-03-11 14:03:23 +0100

    try fix assets rel link

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220311.1353';
+const cacheName = 'chirpy-20220311.1401';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
