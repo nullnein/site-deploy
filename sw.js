e635bbf3b9da6d357f0d6453cb6e894ec05e0205
nullnein <mathis@oija.de>
2022-03-11 14:15:30 +0100

    fixed all assets and build

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220311.1401';
+const cacheName = 'chirpy-20220311.1414';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
