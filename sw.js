6415d9c4cfda768c4c71025d4bb7223ae40a1610
nullnein <mathis@oija.de>
2022-07-24 16:17:12 +0200

    added DW posts and fixed post footer, no licensing, no sharing buttons

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220607.1302';
+const cacheName = 'chirpy-20220724.1615';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
