d0c5301be9cefb0a362887273071d40d2da1f612
nullnein <mathis@oija.de>
2022-11-14 08:19:50 +0100

    Two September posts added

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220904.1240';
+const cacheName = 'chirpy-20221114.0818';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
