b909fa2cdd74e0d2356b7f431b534ae00776b302
nullnein <mathis@oija.de>
2022-12-27 21:58:30 +0100

    edits, river

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221114.0818';
+const cacheName = 'chirpy-20221227.2155';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
