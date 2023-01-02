307aaaa68f517a518b49c32435506aa22b971767
nullnein <mathis@oija.de>
2023-01-02 17:00:10 +0100

    some edits and pinned mathis post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230102.1605';
+const cacheName = 'chirpy-20230102.1659';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
