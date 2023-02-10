aeb4c34f4335598a5fa5eb5fd6a433c999a5123b
nullnein <mathis@oija.de>
2023-02-10 16:33:25 +0100

    added one post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230205.1810';
+const cacheName = 'chirpy-20230210.1632';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
