adcbf417b197c8c5e5732d8b20599ea58631ac9a
nullnein <mathis@oija.de>
2023-07-10 11:19:53 -0400

    added ai reporter blog and paper

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230710.0551';
+const cacheName = 'chirpy-20230710.1718';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
